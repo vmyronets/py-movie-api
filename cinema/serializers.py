941d713d5ff3bbf5b042720047b58bbb6820b99c
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-06-14 21:08:48 +0300

    Solution

---
+++ cinema/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from cinema.models import Movie
+
+
+class MovieSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(max_length=255, required=True)
+    description = serializers.CharField(max_length=255, required=False)
+    duration = serializers.IntegerField(required=False)
+
+    def create(self, validated_data):
+        return Movie.objects.create(**validated_data)
+
+    def Update(self, instance, validated_data):
+        instance.title = validated_data.get("title", instance.title)
+        instance.description = validated_data.get("description", instance.description)
+        instance.duration = validated_data.get("duration", instance.duration)
+        instance.save()
+        return instance
